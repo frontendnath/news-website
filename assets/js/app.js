4aeca16f90157e65fd7e8557422b7aec34c6e4dd
frontendnath <nathanjoe.dev@gmail.com>
2023-04-30 12:26:13 +0100

    code update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,66 @@
 
 const apiData = {
 
-  apiKey: a522b96ab3d14dd5a0778a085e2703f5,
-  baseUrl: "akadsfakdf",
+  apiKey: 'pub_2143166a94aeb909af273e669c3e25fc5cfa3',
+  baseUrl: 'https://newsdata.io/api/1/'
+
+};
+
+const contentNode = document.getElementById('contentNode');
+
+const searchForm = document.getElementById('searchForm');
+
+searchForm.onsubmit = (e) => {
+
+  e.preventDefault()
+
+  const searchInputValue = document.getElementById('searchInput').value;
+  
+  console.log(searchInputValue);
+
+};
+
+getArticles(`${apiData.baseUrl}news?apiKey=${apiData.apiKey}`);
+
+async function getArticles(url) {
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+
+  showArticles(data);
+
+}
+
+function showArticles(articles) {
+
+  contentNode.innerHTML = '';
+
+
+
+  const articleDataInArray = [articles];
+  
+  console.log(articleDataInArray)
+
+  // articleDataInArray.push(articles.articles);
+
+  // articleDataInArray.forEach((data) => {
+
+  //   const articleMainEl = document.createElement('div');
+  //   articleMainEl.classList.add('col-md-4', 'mt-3');
+
+  //   articleMainEl.innerHTML = `
+    
+  //   <div>
+  //     <img src=${data[0].urlToImage} width="100%" />
+  //   </div>
+
+  //   `
+
+  //   console.log(data)
+
+  //   contentNode.appendChild(articleMainEl);
+
+  // });
 
 }
