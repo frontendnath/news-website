926af4cec697e995d4cfe95490fa09605c1c51ae
frontendnath <nathanjoe.dev@gmail.com>
2023-05-01 14:18:47 +0100

    code update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,66 +2,189 @@
 
 const apiData = {
 
-  apiKey: 'pub_2143166a94aeb909af273e669c3e25fc5cfa3',
-  baseUrl: 'https://newsdata.io/api/1/'
-
-};
-
-const contentNode = document.getElementById('contentNode');
-
-const searchForm = document.getElementById('searchForm');
-
-searchForm.onsubmit = (e) => {
-
-  e.preventDefault()
-
-  const searchInputValue = document.getElementById('searchInput').value;
-  
-  console.log(searchInputValue);
-
-};
-
-getArticles(`${apiData.baseUrl}news?apiKey=${apiData.apiKey}`);
-
-async function getArticles(url) {
-
-  const res = await fetch(url);
-  const data = await res.json();
-
-
-  showArticles(data);
+  apiKey: 'a522b96ab3d14dd5a0778a085e2703f5',
+  baseUrl: 'https://newsapi.org/v2/everything',
+  baseUrlForHeadlines: 'https://newsapi.org/v2/top-headlines'
 
 }
 
-function showArticles(articles) {
 
-  contentNode.innerHTML = '';
+// getting headline news
+
+const headlineUrl = `${apiData.baseUrlForHeadlines}?country=ng&apiKey=${apiData.apiKey}`
+
+getHeadlines(headlineUrl);
+
+async function getHeadlines(headlineData) {
+
+  const res = await fetch(headlineData);
+  const headlineNewsData = await res.json();
+
+  showHeadlines(headlineNewsData);
+
+}
+
+function showHeadlines(apiHeadlineData) {
+
+  const headlineNews = [...apiHeadlineData.articles];
+
+  headlineNews.forEach((data) => {
+
+    const extractedHeadlineNews = [data];
+
+    const headlineNewsEl = document.getElementById('headlineNewsEl');
+
+    const spanElement = document.createElement('span');
+
+    spanElement.classList.add('me-2');
+
+    extractedHeadlineNews.forEach((item) => {
+
+      spanElement.innerHTML = `${item.title}.`
+
+    });
+
+    headlineNewsEl.appendChild(spanElement);
+
+  });
+
+}
 
 
+// getting first three headline news 
 
-  const articleDataInArray = [articles];
+const nodeElement = document.getElementById('nodeElement');
+
+const firstThreeNewsUrl = `${apiData.baseUrl}?q=tech&apiKey=${apiData.apiKey}`;
+
+async function getFirstThreeNews(firstThreeNewsData) {
+
+  const res = await fetch(firstThreeNewsData);
+  const responseData = await res.json();
+
+  showFirstThree(responseData);
+
+}
+
+getFirstThreeNews(firstThreeNewsUrl);
+
+function showFirstThree(apiFirstThreeData) {
+
+  console.log(apiFirstThreeData)
+
+  const firstNews = document.createElement('div');
+  firstNews.classList.add('col-md-5');
+
+  firstNews.innerHTML = `
   
-  console.log(articleDataInArray)
+    <div>
 
-  // articleDataInArray.push(articles.articles);
+      <h3 class="text-dark fw-bold">
+        ${apiFirstThreeData.articles[0].title}
+      </h3>
 
-  // articleDataInArray.forEach((data) => {
+      <div class="img_box">
+        <img src=${apiFirstThreeData.articles[0].urlToImage} width="100%" />
+      </div>
 
-  //   const articleMainEl = document.createElement('div');
-  //   articleMainEl.classList.add('col-md-4', 'mt-3');
+      <div class="mt-3">
+        <div class="small text-muted">
+          <i class="far fa-user me-2"></i> Author: ${apiFirstThreeData.articles[0].author}
+        </div>
 
-  //   articleMainEl.innerHTML = `
-    
-  //   <div>
-  //     <img src=${data[0].urlToImage} width="100%" />
-  //   </div>
+        <h6 class="text-dark mt-2 border-bottom pb-3">
+          ${apiFirstThreeData.articles[0].description}
+        </h6>
 
-  //   `
+        <div class="mt-2">
+          <p class="text-muted small">
+            ${apiFirstThreeData.articles[0].content}
+          </p>
+        </div>
 
-  //   console.log(data)
+        <div class="mt-3">
+          <a href=${apiFirstThreeData.articles[0].url} target="_blank" class="text-primary text-decoration-none d-flex align-items-center">
+            Read more <i class="fas fa-arrow-right text-decoration-none ms-2"></i>
+          </a>
+        </div>
+      </div>
 
-  //   contentNode.appendChild(articleMainEl);
+    </div>
 
-  // });
+  `;
+
+  const secondNews = document.createElement('div');
+  secondNews.classList.add('col-md-4');
+
+  secondNews.innerHTML = `
+  
+    <div>
+      <div class="img_box">
+        <img src=${apiFirstThreeData.articles[1].urlToImage} width="100%" />
+      </div>
+
+      <div class="mt-3">
+        <div class="small text-muted">
+          <i class="far fa-user me-2"></i> Author: ${apiFirstThreeData.articles[1].author}
+        </div>
+
+        <h6 class="text-dark mt-2 border-bottom pb-3">
+          ${apiFirstThreeData.articles[1].description}
+        </h6>
+
+        <div class="mt-2">
+          <p class="text-muted small">
+            ${apiFirstThreeData.articles[1].content}
+          </p>
+        </div>
+
+        <div class="mt-3">
+          <a href=${apiFirstThreeData.articles[1].url} target="_blank" class="text-primary text-decoration-none d-flex align-items-center">
+            Read more <i class="fas fa-arrow-right text-decoration-none ms-2"></i>
+          </a>
+        </div>
+      </div>
+
+    </div>
+
+  `;
+
+  const thirdNews = document.createElement('div');
+  thirdNews.classList.add('col-md-3');
+
+  thirdNews.innerHTML = `
+  
+    <div>
+      <div class="img_box">
+        <img src=${apiFirstThreeData.articles[2].urlToImage} width="100%" />
+      </div>
+
+      <div class="mt-3">
+        <div class="small text-muted">
+          <i class="far fa-user me-2"></i> Author: ${apiFirstThreeData.articles[2].author}
+        </div>
+
+        <h6 class="text-dark mt-2 border-bottom pb-3">
+          ${apiFirstThreeData.articles[2].description}
+        </h6>
+
+        <div class="mt-2">
+          <p class="text-muted small">
+            ${apiFirstThreeData.articles[2].content}
+          </p>
+        </div>
+
+        <div class="mt-3">
+          <a href=${apiFirstThreeData.articles[2].url} target="_blank" class="text-primary text-decoration-none d-flex align-items-center">
+            Read more <i class="fas fa-arrow-right text-decoration-none ms-2"></i>
+          </a>
+        </div>
+      </div>
+
+    </div>
+
+  `;
+
+  nodeElement.append(firstNews, secondNews, thirdNews);
 
 }
